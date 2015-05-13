d1a53df0e2cac24e214d40b3e88f3174fe8ee5fa
Henry Zhu <hi@henryzoo.com>
2015-05-13 08:57:23 -0400

    RulePageView: add link to rule/test source files
    Fixes #8

--- app/views/pages/RulePageView/index.jsx
+++ app/views/pages/RulePageView/index.jsx
@@ -4,6 +4,11 @@ import locationStore from '../../../stores/locationStore';
 import PageView from '../../PageView';
 import TitleView from '../../TitleView';
 
+const baseUrl = 'https://github.com/jscs-dev/node-jscs';
+const getRuleUrl = ruleName => `${baseUrl}/blob/master/lib/rules/${ruleName}.js`;
+const getTestUrl = ruleName => `${baseUrl}/blob/master/test/specs/rules/${ruleName}.js`;
+const setCamelCaseToHyphens = str => str.replace(/([a-z][A-Z])/g, (g) => `${g[0]}-${g[1].toLowerCase()}`);
+
 export default React.createClass({
     render() {
         var ruleName = locationStore.getData();
@@ -20,6 +25,8 @@ export default React.createClass({
                          dangerouslySetInnerHTML={{__html: rule.getDescription()}}>
                     </div>
                 </div>
+                <div><a href={getRuleUrl(setCamelCaseToHyphens(rule.getName()))}>Rule source</a></div>
+                <div><a href={getTestUrl(setCamelCaseToHyphens(rule.getName()))}>Test source</a></div>
             </PageView>
         )
     }
