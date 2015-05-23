91606e32ad5afaf02d0ca61c05759d19b75df5bd
Marat Dulin <mdevils@gmail.com>
2015-05-23 14:46:12 +0200

    Fix for location rewriting

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import AppView from './views/AppView';
-import JscsModel from './models/JscsModel';
 import dataStore from './stores/dataStore';
+import JscsModel from './models/JscsModel';
 
 if (typeof window.__jscsData !== 'undefined') {
     dataStore.setData(JscsModel.fromJSON(window.__jscsData));
 }
+
+import AppView from './views/AppView';
 
 React.render(<AppView />, document.getElementById('root'));
