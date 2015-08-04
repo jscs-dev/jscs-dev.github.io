5cf381dc79e0614f8df7b002a1fbb5718927b862
Eldad Bercovici <dutzi.b@gmail.com>
2015-08-04 18:33:11 +0300

    Added filtering to rules list

--- app/views/pages/RuleListPageView/index.jsx
+++ app/views/pages/RuleListPageView/index.jsx
@@ -8,13 +8,37 @@ import TitleView from '../../TitleView';
 import './style.styl';
 
 export default React.createClass({
+    getInitialState: function() {
+        return {
+            search: ''
+        };
+    },
+    componentDidMount: function() {
+        React.findDOMNode(this.refs.search).focus();
+    },
+    onSearchChange() {
+        this.setState({
+            search: new RegExp(React.findDOMNode(this.refs.search).value, 'i')
+        });
+        this.render();
+    },
     render() {
         return (
             <PageView>
                 <TitleView>Rules</TitleView>
+                <input 
+                    type="text"
+                    className="search-bar"
+                    ref="search"
+                    placeholder="Filter: e.g, 'MultipleSpaces'..."
+                    onChange={this.onSearchChange}/>
                 <ul className="rule-list">
                     {dataStore.getData().getRules()
                         .filter((rule) => Boolean(rule.getShortDescription()))
+                        .filter((rule) => {
+                            return rule.getShortDescription().match(this.state.search) ||
+                                rule.getName().match(this.state.search);
+                        })
                         .map((rule) => {
                             return <RuleListItem
                                 key={rule.getFilename()}
